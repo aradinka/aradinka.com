5973d882f697cb82cbb84722e2abc0ec28ee95f6
aradinka <71642722+aradinka@users.noreply.github.com>
2022-11-02 23:43:05 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home({ posts }) {
         </div>
         <p className="space-y-4 text-zinc-500 dark:text-slate-300">
           <p className="my-4 leading-loose">
-            Graduated from a Bachelor in Statistics at{' '}
+            Graduated from a Bachelor in Statistics at {' '}
             <a
               className="focusable rounded-sm font-medium text-zinc-800 underline decoration-blue-500 decoration-2 underline-offset-2 transition duration-100 hover:text-blue-500 hover:decoration-sky-500/30 focus:text-blue-500 focus:ring-sky-500/40 dark:text-white dark:decoration-sky-400 dark:hover:text-sky-400 dark:hover:decoration-sky-400/30 dark:focus:text-sky-400 dark:focus:ring-sky-400/40"
               href="https://www.its.ac.id/statistika/en/home/"
@@ -41,16 +41,16 @@ export default function Home({ posts }) {
             >
               Sepuluh Nopember Instute of Technology
             </a>
-            | I have {' '}
+            I have {' '}
             <a
               className="focusable rounded-sm font-medium text-zinc-800 underline decoration-blue-800 decoration-2 underline-offset-2 transition duration-100 hover:text-blue-800 hover:decoration-blue-800/30 focus:text-blue-500 focus:ring-blue-500/40 dark:text-white dark:decoration-sky-400 dark:hover:text-sky-400 dark:hover:decoration-sky-400/30 dark:focus:text-sky-400 dark:focus:ring-sky-400/40"
               href="https://www.instagram.com/p/CZZFAYovC8x/?utm_source=ig_web_copy_link"
               rel="noreferrer"
               target="_blank"
             >
-              one year data science internship experience
+              one year data science internship
             </a>{' '}
-            in technical AI team DDB {' '}
+            experience technical AI team DDB {' '}
             <a
               className="focusable rounded-sm font-medium text-zinc-800 underline decoration-green-500 decoration-2 underline-offset-2 transition duration-100 hover:text-green-500 hover:decoration-green-500/30 focus:text-lime-500 focus:ring-lime-500/40 dark:text-white dark:decoration-lime-400 dark:hover:text-lime-400 dark:hover:decoration-lime-400/30 dark:focus:text-lime-400 dark:focus:ring-lime-400/40"
               href="https://www.telkom.co.id/sites"
@@ -59,14 +59,14 @@ export default function Home({ posts }) {
             >
               Telkom Indonesia
             </a>{' '}
-            and one year experience as a computing lab assistant teaching data analytics course | I graduate from {' '}
+            and one year experience as a computing lab assistant teaching data analytics course I graduate from {' '}
             <a
               className="focusable rounded-sm font-medium text-zinc-800 underline decoration-sky-700 decoration-2 underline-offset-2 transition duration-100 hover:text-sky-700 hover:decoration-sky-500/30 focus:text-orange-500 focus:ring-sky-500/40 dark:text-white dark:decoration-blue-400 dark:hover:text-blue-400 dark:hover:decoration-blue-400/30 dark:focus:text-blue-400 dark:focus:ring-blue-400/40"
               href="https://grow.google/intl/id_id/bangkit/"
               rel="noreferrer"
               target="_blank"
             >
-              Google Bangkit 2021
+              Google Bangkit
             </a>
             and getting my {' '}
             <a
