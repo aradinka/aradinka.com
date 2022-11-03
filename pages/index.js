883f9d1bbafc52282ddc107140b16ee254a162c4
aradinka <azkaradinka@gmail.com>
2022-11-03 20:52:23 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -162,8 +162,7 @@ export default function Home({ posts }) {
             <div className="flex min-w-0 flex-col justify-center">
               <p className="mb-1 flex items-center">
                 <span className="truncate font-semibold text-zinc-700 dark:text-zinc-100">
-                Sepuluh Nopember Institute of 
-                Technology
+                  Sepuluh Nopember Institute of Technology
                 </span>
                 <span className="ml-1.5 inline-block flex-none translate-y-px rounded bg-zinc-100 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
                   Feb, 2022
