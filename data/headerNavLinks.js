ff8abc48498aef88f1af402a14403dd629069567
aradinka <azkaradinka@gmail.com>
2022-11-04 14:44:07 +0700

    Remove photography section

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'All Post' },
   // { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/photography', title: 'Photography' },
+  // { href: '/photography', title: 'Photography' },
   // { href: '/about', title: 'About' },
 ]
 
