33d169dfdb7150e37507fa0ace5523698071c752
aradinka <azkaradinka@gmail.com>
2022-11-14 05:44:22 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -170,7 +170,15 @@ export default function Home({ posts }) {
               </p>
             </div>
             <div className="mb-2">
-              <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+              <iframe 
+              src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI&font=Default&lang=en&initial_zoom=2&height=650'
+              width='100%' 
+              height='650'
+              webkitallowfullscreen
+              mozallowfullscreen
+              allowfullscreen
+              frameborder='0'>
+              </iframe>
             </div>
           </li>
         </ul>
