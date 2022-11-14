d6c4b84794198aaf8b5b869a186671ccc623355f
aradinka <azkaradinka@gmail.com>
2022-11-14 15:26:55 +0700

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/projects', title: 'Projects' },
   { href: '/photography', title: 'Photography' },
   // { href: '/about', title: 'About' },
-  { href: '/journey', title: 'Journey' },
+  // { href: '/journey', title: 'Journey' },
 ]
 
 export default headerNavLinks
