3459d1610a9b984e6f57bdb39540a4ae7c182f3c
aradinka <azkaradinka@gmail.com>
2022-11-17 11:36:15 +0700

    Add bangkit 2021 experience

--- pages/index.js
+++ pages/index.js
@@ -93,6 +93,39 @@ export default function Home({ posts }) {
       <div className="content mt-12 sm:mt-24">
         <h2 className="mb-2 text-xl font-bold text-zinc-800 dark:text-white">Experiences</h2>
         <ul className="mt-8">
+        <li className="dark:text-zinc-350 my-5 flex items-center gap-4 text-zinc-500">
+            <a
+              className="link focusable font-medium text-zinc-800 dark:text-white"
+              href="https://grow.google/intl/id_id/bangkit/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <Image
+                src="/static/images/company/bangkit-logo.jpeg"
+                alt="Google Bangkit 2021"
+                width="50px"
+                height="50px"
+                className="rounded-md"
+              />
+            </a>
+            <div className="flex min-w-0 flex-col justify-center">
+              <p className="mb-1 flex items-center">
+                <span className="truncate font-semibold text-zinc-700 dark:text-zinc-100">
+                  Google Bangkit 2021
+                </span>
+                <span className="ml-1.5 inline-block flex-none translate-y-px rounded bg-zinc-100 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+                  Feb 2021
+                  <span className="text-zinc-350 dark:text-zinc-550 mx-0.5">—</span>
+                  Jun 2022
+                </span>
+              </p>
+              <p className="flex items-center truncate">
+                <span className="flex-1 truncate text-zinc-500 dark:text-zinc-400">
+                  Machine Learning Students
+                </span>
+              </p>
+            </div>
+          </li>          
           <li className="dark:text-zinc-350 my-5 flex items-center gap-4 text-zinc-500">
             <a
               className="link focusable font-medium text-zinc-800 dark:text-white"
