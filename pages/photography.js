940e4530ba440d8f2642c54a62399693843cb2c6
aradinka <azkaradinka@gmail.com>
2022-11-18 15:08:29 +0700

    Update photography.js

--- pages/photography.js
+++ pages/photography.js
@@ -8,11 +8,6 @@ export default function Photography() {
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Photography Reels
-          </h1>
-        </div>
         <div className="mb-2">
           <iframe
             src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1TBHh1Ki2_UJtOGNn9h9NrFE_dfHJLNtOKFwN3Yo09KQ&font=Default&lang=en&initial_zoom=2&height=650"
