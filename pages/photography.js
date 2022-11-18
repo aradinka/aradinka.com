e58d23eb3e6c872f5a5be8cbd23758dcef8dccdf
aradinka <azkaradinka@gmail.com>
2022-11-18 15:04:47 +0700

    Add photography reels

--- pages/photography.js
+++ pages/photography.js
@@ -10,11 +10,19 @@ export default function Photography() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Photography
+            Photography Reels
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            My Photography Collection
-          </p>
+        </div>
+        <div className="mb-2">
+          <iframe
+            src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1TBHh1Ki2_UJtOGNn9h9NrFE_dfHJLNtOKFwN3Yo09KQ&font=Default&lang=en&initial_zoom=2&height=650"
+            width="100%"
+            height="650"
+            webkitallowfullscreen
+            mozallowfullscreen
+            allowFullScreen
+            frameBorder="0"
+          ></iframe>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
