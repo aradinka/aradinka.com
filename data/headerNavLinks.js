33ceeceabeedcfd2593f88f0d62dc1dc821c0b39
aradinka <azkaradinka@gmail.com>
2022-11-20 21:59:50 +0700

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   //{ href: '/post', title: 'Blog' },
-  // { href: '/tags', title: 'Tags' },
+  { href: '/tags', title: 'Package' },
   { href: '/projects', title: 'Projects' },
   { href: '/photography', title: 'Photography' },
   // { href: '/about', title: 'About' },
