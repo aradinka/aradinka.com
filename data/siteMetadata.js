06a74dd8de485fbec3b3befc75887d07b3b8c645
aradinka <azkaradinka@gmail.com>
2022-11-29 13:16:33 +0700

    update web thumbnail

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/aradinka/tailwind-nextjs-web',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/web-thumbnail.png',
   email: 'azkaradinka@gmail.com',
   github: 'https://github.com/aradinka',
   // twitter: 'https://twitter.com/Twitter',
